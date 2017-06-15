1cb376582fd7d80d0555d602176c8a709e44b2e0
licdar87 <licdar87@gmail.com>
2017-06-15 17:44:01 +0200

    Pagina prodotti
    Versione di base

--- resources/views/layout/frontend/partials/navbar.blade.php
+++ resources/views/layout/frontend/partials/navbar.blade.php
@@ -2,6 +2,7 @@
 
 @section('pagestyle')
 
+  @yield('subpagestyle')
 <!-- IE10 viewport hack for Surface/desktop Windows 8 bug 
 <link href="http://getbootstrap.com/assets/css/ie10-viewport-bug-workaround.css" 
         rel="stylesheet">
