98185dc134b005e923799dbb7e264d3d5d30ffac
devsu88 <developer.su120188@gmail.com>
2017-06-20 20:36:19 +0200

    updates create and edit views shops

--- storage/framework/views/8d949b9657db321ad4437e1d34135b440f0314ac.php
+++
@@ -1,7 +0,0 @@
-<?php if(Session::has('message')): ?>
-    <div class="alert alert-info alert-dismissable">
-	  <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-	  <?php echo e(Session::get('message')); ?>
-
-	</div>
-<?php endif; ?>
